55c3f9d08dc2f6c907e5225f8cdffe2248e5a5e2
zyfu <zyfu0623@163.com>
2019-04-01 20:50:42 +0800

    数据结构代码

---
+++ src/main/java/com/zyfu/day01/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package com.zyfu.day01;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> str = new ArrayList<String>();
+
+        List<Integer> list = new LinkedList<Integer>();
+    }
+}
